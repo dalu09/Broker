bd46ff3369a2a8050c1f8baec6dc75e7b6a91ed7
Lucia Castillo <111028118+dalu09@users.noreply.github.com>
2024-10-09 00:57:00 -0500

    Broker listo para desplegar

--- broker/broker/urls.py
+++ broker/broker/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import broker_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/broker/servicio1/', broker_view),
 ]
+
---
+++ broker/broker/views.py
@@ -0,0 +1,17 @@
+import requests
+from django.http import JsonResponse
+
+def broker_view(request):
+    service_url = ""
+
+    if 'servicio1' in request.path:
+        #Cambiar ip
+        service_url = "http://ip_maquina_servicio1.com/api/endpoint/"
+
+    session = requests.Session()
+
+    try:
+        response = session.get(service_url, params=request.GET, timeout=5)  
+        return JsonResponse(response.json(), status=response.status_code)
+    except requests.exceptions.RequestException as e:
+        return JsonResponse({"error": "No se pudo conectar al servicio"}, status=500)

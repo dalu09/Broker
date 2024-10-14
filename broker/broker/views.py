e3e68264e48b84aa7e3839b1514dbbe5d3d3df7b
Juan Camilo Beltran <111070585+Juancabel@users.noreply.github.com>
2024-10-14 12:12:21 -0500

    Update views.py

--- broker/broker/views.py
+++ broker/broker/views.py
@@ -15,3 +15,18 @@ def broker_view(request):
         return JsonResponse(response.json(), status=response.status_code)
     except requests.exceptions.RequestException as e:
         return JsonResponse({"error": "No se pudo conectar al servicio"}, status=500)
+
+def broker_post(request):
+    service_url = ""
+
+    if 'servicio2' in request.path:
+        #Cambiar ip
+        service_url = "http://ip_maquina_servicio1.com/api/endpoint/"
+
+    session = requests.Session()
+
+    try:
+        response = session.get(service_url, params=request.POST, timeout=5)  
+        return JsonResponse(response.json(), status=response.status_code)
+    except requests.exceptions.RequestException as e:
+        return JsonResponse({"error": "No se pudo conectar al servicio"}, status=500)

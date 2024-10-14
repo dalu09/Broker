f1b367adfec5748f4218033e7c3fe47a7d3f7fb0
Juan Camilo Beltran <111070585+Juancabel@users.noreply.github.com>
2024-10-14 12:18:08 -0500

    Update urls.py

--- broker/broker/urls.py
+++ broker/broker/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import broker_view
+from .views import broker_post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
